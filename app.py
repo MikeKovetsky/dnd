fbd414c4e437057c8f3462abf616f76e1f076971
mkovetsky <mkovetsky@gmail.com>
2023-11-07 21:51:21 +0100

    mike: init

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from openai import OpenAI
+
+client = OpenAI(api_key=os.environ["OPENAI_KEY"])
+app = FastAPI()
+
+
+class ImageRequest(BaseModel):
+    prompt: str
+
+
+@app.post("/generate-image/")
+async def generate_image(request: ImageRequest):
+    if not request.prompt:
+        raise HTTPException(status_code=400, detail="Prompt is required")
+
+    # Call the OpenAI API to generate the image
+    try:
+
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=request.prompt,
+            size="1024x1024",
+            quality="standard",
+            n=1,
+        )
+
+        image_url = response.data[0].url
+    except Exception as e:
+        # Handle exceptions (e.g., network error, invalid API key)
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"image_url": image_url}
+
+
+# Dummy run command for the FastAPI app
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
